b155d6308322e7155509a0f50a2eb81b5af8637f
chip2n <andreas@arvidsson.io>
2024-05-17 14:03:12 +0200

    Update zig version

--- chip/packages/zig.scm
+++ chip/packages/zig.scm
@@ -7,12 +7,12 @@
 (define-public zig
   (package
    (name "zig")
-   (version "0.12.0-dev.3644+05d975576")
+   (version "0.13.0-dev.211+6a65561e3")
    (source (origin
             (method url-fetch)
             (uri (string-append "https://ziglang.org/builds/zig-linux-x86_64-" version ".tar.xz"))
             (sha256
-             (base32 "15ymq89gfcmg0apywx8xs4vjf2cvjnppv5a904zagha4yw7dkr4r"))))
+             (base32 "05njpppvyx9pchx9hca1argph2n933cx0h1jn4mzd27pkynksswx"))))
    (build-system copy-build-system)
    (arguments
     `(#:install-plan '(("." "bin/"))))
